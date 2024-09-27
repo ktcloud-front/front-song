7236acd6bb119d701ea631466ef2f912664a14f4
82244872 <j.song@kt.com>
2024-09-27 15:42:56 +0900

    test2

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -8,9 +8,9 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'index',
+      name: 'ui',
       formats: ['es', 'cjs'],
-      fileName: (format) => `index.${format}.js`,
+      fileName: (format) => `ui.${format}.js`,
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
