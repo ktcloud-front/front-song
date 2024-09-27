cc6839b3c07bc70c636df2a78390e89436f7a46e
82244872 <j.song@kt.com>
2024-09-27 16:48:47 +0900

    test5

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path'
-import dfs from 'vite-plugin-dts'
-import dtsPlugin from 'vite-plugin-dts'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dfs()],
+  plugins: [react(), dts()],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
