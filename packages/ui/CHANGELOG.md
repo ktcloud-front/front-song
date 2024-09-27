030bfeeea0e2d575d1a5015a407c7b9e2c5094b7
82244872 <j.song@kt.com>
2024-09-27 15:24:37 +0900

    first-test

---
+++ packages/ui/CHANGELOG.md
@@ -0,0 +1,19 @@
+# @jaeheesong-ktc/ui
+
+## 2.0.0
+
+### Major Changes
+
+- test6
+
+## 1.0.0
+
+### Major Changes
+
+- test
+
+## 0.1.0
+
+### Minor Changes
+
+- init
---
+++ packages/ui/vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import * as path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'index',
+      formats: ['es', 'cjs'],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+})
