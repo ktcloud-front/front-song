033afa75e17eb270c4f6227f8bdbba95fa540c06
82244872 <j.song@kt.com>
2024-09-30 09:56:58 +0900

    test13

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path'
-// import dts from 'vite-plugin-dts'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({insertTypesEntry: true})],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'ui-design-system',
+      formats: ['es', 'cjs'],
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
